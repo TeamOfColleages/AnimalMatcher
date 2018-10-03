cbb392e65a1509c0a5a9a8340c56c0418a3e22ac
tcholakov <tcholakov2@gmail.com>
2018-10-03 22:21:29 +0300

    Added Web ASP.NET Core project with Database Identity and a separate project for Data

---
+++ AnimalMatcher/AnimalMatcher.Data/AnimalMatcherDbContext.cs
@@ -0,0 +1,13 @@
+﻿namespace AnimalMatcher.Data
+{
+    using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+    using Microsoft.EntityFrameworkCore;
+
+    public class AnimalMatcherDbContext : IdentityDbContext
+    {
+        public AnimalMatcherDbContext(DbContextOptions<AnimalMatcherDbContext> options)
+            : base(options)
+        {
+        }
+    }
+}
