809ce22abfc115e83344f9985385f4da5771d796
tcholakov <tcholakov2@gmail.com>
2018-10-04 08:51:45 +0300

    Removed AnimalMatcher.Infrastructure project and moved the AutoMapper profile to the AnimalMatcher.Web project

---
+++ AnimalMatcher/AnimalMatcher.Web/Infrastructure/Mapping/AutoMapperProfile.cs
@@ -0,0 +1,9 @@
+﻿namespace AnimalMatcher.Web.Infrastructure.Mapping
+{
+    using AutoMapper;
+
+    public class AutoMapperProfile : Profile
+    {
+        public AutoMapperProfile() { }
+    }
+}
