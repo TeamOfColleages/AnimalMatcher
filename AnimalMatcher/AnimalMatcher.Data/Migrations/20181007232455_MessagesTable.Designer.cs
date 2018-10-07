3231d92130afe830a18c3e78b70703970075b7a1
Chris Deaker <>
2018-10-08 12:29:52 +1300

    Add EF migration for Message table.

--- AnimalMatcher/AnimalMatcher.Data/Migrations/20181007232455_MessagesTable.Designer.cs
+++ AnimalMatcher/AnimalMatcher.Data/Migrations/20181007232455_MessagesTable.Designer.cs
@@ -4,14 +4,16 @@ using AnimalMatcher.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace AnimalMatcher.Data.Migrations
 {
     [DbContext(typeof(AnimalMatcherDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20181007232455_MessagesTable")]
+    partial class MessagesTable
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
