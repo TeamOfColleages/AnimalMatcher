a826eb8a7f54d351615433f6a15d9e78d2d07676
tcholakov <tcholakov2@gmail.com>
2018-10-15 09:05:28 +0300

    Refactored AutoMapperProfile by separating it into the AutoMapperWebProfile and AutoMapperServicesProfile every class is now responsible for its own project for better separation of concerns

--- AnimalMatcher/AnimalMatcher.Web/Infrastructure/Mapping/AutoMapperWebProfile.cs
+++ AnimalMatcher/AnimalMatcher.Web/Infrastructure/Mapping/AutoMapperWebProfile.cs
@@ -1,53 +1,31 @@
 ﻿namespace AnimalMatcher.Web.Infrastructure.Mapping
 {
-    using AnimalMatcher.Data.Models;
-    using AnimalMatcher.Services.Location.Interfaces;
-    using AnimalMatcher.Services.Models.Location;
     using AnimalMatcher.Services.Models.Owner;
     using AnimalMatcher.Services.Models.Pet;
     using AnimalMatcher.Web.Models.Owner;
     using AnimalMatcher.Web.Models.Pet;
     using AutoMapper;
 
-    public class AutoMapperProfile : Profile
+    public class AutoMapperWebProfile : Profile
     {
-        public AutoMapperProfile()
+        public AutoMapperWebProfile()
         {
             this.PetRegistrations();
             this.OwnerRegistrations();
-            this.LocationRegistrations();
         }
 
         private void PetRegistrations()
         {
-            this.CreateMap<PetRegisterServiceModel, Pet>();
-
             this.CreateMap<PetInputModel, PetRegisterServiceModel>()
                 .ForPath(petServiceModel => petServiceModel.Location.Latitude, cfg => cfg.MapFrom(petInputModel => petInputModel.Latitude))
                 .ForPath(petServiceModel => petServiceModel.Location.Longitude, cfg => cfg.MapFrom(petInputModel => petInputModel.Longitude));
 
             this.CreateMap<PetWithOwnerServiceModel, PetDetailedViewModel>();
-
-            this.CreateMap<Pet, PetWithOwnerServiceModel>();
-
-            this.CreateMap<Pet, PetServiceModel>();
-
-            this.CreateMap<Pet, PetWithDistanceServiceModel>();
         }
 
         private void OwnerRegistrations()
         {
-            this.CreateMap<Owner, OwnerServiceModel>()
-                .ForMember(ownerServiceModel => ownerServiceModel.Username, cfg => cfg.MapFrom(ownerDataModel => ownerDataModel.UserName));
-
-            this.CreateMap<Owner, OwnerWithPetsServiceModel>();
-
             this.CreateMap<OwnerWithPetsServiceModel, OwnerViewModel>();
-        }
-
-        private void LocationRegistrations()
-        {
-            this.CreateMap<LocationDTO, Location>();
         }
     }
 }
